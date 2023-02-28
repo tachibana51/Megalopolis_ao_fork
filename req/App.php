e256ce311d253bf472aff57cdf3aa8719682c493
tachibana51 <piyopiyo51@gmail.com>
2023-02-28 21:35:25 +0900

    chore: comment out get_magic_quotes_gpc()

--- req/App.php
+++ req/App.php
@@ -72,6 +72,8 @@ class App
 
 	static function stripMagicQuotesSlashes()
 	{
+		// not deprecated in php 8.0
+		/*
 		if (get_magic_quotes_gpc())
 		{
 			$_GET = self::stripSlashesRecursive($_GET);
@@ -79,6 +81,7 @@ class App
 			$_REQUEST = self::stripSlashesRecursive($_REQUEST);
 			$_COOKIE = self::stripSlashesRecursive($_COOKIE);
 		}
+		*/
 	}
 
 	private static function stripSlashesRecursive($arg)
